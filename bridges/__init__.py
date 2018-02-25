7eb19ab7daf29203313755f95f7947ff10d1543b
Jeroen Vet <vet@excecbc.com>
2018-02-25 09:36:31 +0100

    first commit

---
+++ bridges/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+#~ import models
+import dbfilterfix
+import product
+import purchase
+import sale
+import partner
---
+++ bridges/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bridges Custom Module',
+    'summary': 'Contains company specific customizatons',
+    'version': '1.0',
+    'category': 'Tools',
+    'description': """
+KE Instruments Custom Module
+===========================================================
+
+Contains company specific customizations and configuration data
+
+    """,
+    'author': 'eXcec Business Consulting Ltd.',
+    'depends': ['account','sale','purchase'],
+    'website': 'http://www.excecbc.com',
+    'data': [
+        'config.xml',
+        'report_paperformat.xml',
+        'layouts.xml',
+        'views.xml',
+        
+    ],
+    'installable': True,
+    'auto_install': False,
+    'images':[],
+    
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
